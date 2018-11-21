4cf053ba8ffa2a5bb3b6366c894e5337895bcbbf
mareenas <persempre1@yandex.ru>
2018-11-21 10:13:14 +0300

    initial

---
+++ src/MyApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class MyApp extends Component {
+  render() {
+    return (
+      <div className="MyApp">
+        <header className="App-header">
+          This is React
+        </header>
+      </div>
+    );
+  }
+}
+
+export default MyApp;
