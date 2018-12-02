4e797a8f7b3946a65e8a30ccf4aecf34560ba9cf
mareenas <persempre1@yandex.ru>
2018-12-02 12:58:32 +0300

    music page

--- src/MusicPage.js
+++ src/MusicPage.js
@@ -3,6 +3,7 @@ import Header from './components/Header.js';
 import MainMusicPage from "./components/MainMusicPage.js";
 import './MyApp.css';
 import './css/MusicPage.css';
+import './audio.min.js';
 
 class MusicPage extends Component {
 
--- src/components/PlaylistsContainer.js
+++ src/components/PlaylistsContainer.js
@@ -1,53 +1,181 @@
 import React, {Component} from 'react';
 import CustomPlaylist from "./CustomPlaylist";
 
-let playlistTitles = ["Green Goo's", "Collin Y's", "Marina V's", "Elaine D's"];
+let playlistInfo = [
+    {
+        title: "Green Goo's",
+        songInfo: [
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+        ]
+    },
+    {
+        title: "Collin Y's",
+        songInfo: [
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+        ]
+    },
+    {
+        title: "Marina V's",
+        songInfo: [
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "3:22"
+            },
+        ]
+    },
+    {
+        title: "Elaine D's",
+        songInfo: [
+            {
+                src: "https://pbs.twimg.com/profile_images/738652880387878912/kEeI_JQZ_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "2:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/738652880387878912/kEeI_JQZ_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "2:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/738652880387878912/kEeI_JQZ_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "2:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/738652880387878912/kEeI_JQZ_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "2:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/738652880387878912/kEeI_JQZ_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "2:22"
+            },
+            {
+                src: "https://pbs.twimg.com/profile_images/738652880387878912/kEeI_JQZ_bigger.jpg",
+                title: "The Past Starts Slow and Ends",
+                artist: "System of a Revenge",
+                timing: "2:22"
+            },
+        ]
+    },
 
-let songInfo = [
-    {
-        src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
-        title: "The Past Starts Slow and Ends",
-        artist: "System of a Revenge",
-        timing: "3:22"
-    },
-    {
-        src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
-        title: "The Past Starts Slow and Ends",
-        artist: "System of a Revenge",
-        timing: "3:22"
-    },
-    {
-        src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
-        title: "The Past Starts Slow and Ends",
-        artist: "System of a Revenge",
-        timing: "3:22"
-    },
-    {
-        src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
-        title: "The Past Starts Slow and Ends",
-        artist: "System of a Revenge",
-        timing: "3:22"
-    },
-    {
-        src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
-        title: "The Past Starts Slow and Ends",
-        artist: "System of a Revenge",
-        timing: "3:22"
-    },
-    {
-        src: "https://pbs.twimg.com/profile_images/980736422176022528/BCamJJE1_bigger.jpg",
-        title: "The Past Starts Slow and Ends",
-        artist: "System of a Revenge",
-        timing: "3:22"
-    },
+
 ];
 
 class PlaylistsContainer extends Component{
     render() {
         return(
             <div className="row" style={{marginBottom: "50px"}}>
-                {playlistTitles.map(function(title, index){
-                    return <CustomPlaylist key={index} title={title} songInfo={songInfo}/>;
+                {playlistInfo.map(function(playlist, index){
+                    return <CustomPlaylist key={index} title={playlist.title} songInfo={playlist.songInfo}/>;
                 })}
             </div>
         )
